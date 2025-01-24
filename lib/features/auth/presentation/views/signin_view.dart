57cf756a84bd588d03535406f1b25a000e5ec695
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-01-25 01:14:11 +0200

    Add SigninView and SigninViewBody for authentication screen

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:in_pocket/features/auth/presentation/views/widgets/signin_view_body.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
   static const String routeName = 'signin';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SigninViewBody(),
+    );
   }
 }
