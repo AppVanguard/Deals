a4c1524f06891e42f2abed7a845b6b663ba08a27
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-01-27 18:43:10 +0200

    Remove unused localization import from CustomPhoneField

--- lib/features/auth/presentation/views/widgets/custom_phone_field.dart
+++ lib/features/auth/presentation/views/widgets/custom_phone_field.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:in_pocket/core/utils/app_colors.dart';
 import 'package:in_pocket/core/utils/app_text_styles.dart';
-import 'package:in_pocket/generated/l10n.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:intl_phone_field/phone_number.dart';
 
