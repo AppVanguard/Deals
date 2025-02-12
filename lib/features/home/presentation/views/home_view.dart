45ca91f2175d4403a9fdc0f7376b75d248552a31
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-02-12 22:41:54 +0200

    Add BottomAppBar to HomeView for improved navigation

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -18,6 +18,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomAppBar(),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         leading: Builder(
