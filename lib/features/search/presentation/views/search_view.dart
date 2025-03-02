d9d00d4bbddde72557a805436713de074066fa39
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-03-02 17:29:18 +0200

    Refactor SearchView constructor to use shorthand syntax for key parameter

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,15 +1,13 @@
 // lib/features/search/presentation/views/search_view.dart
 
 import 'package:deals/features/search/presentation/views/widgets/build_search_app_bar.dart';
-import 'package:deals/features/search/presentation/views/widgets/filter_dialog/filter_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:deals/core/utils/app_images.dart';
 import 'package:deals/features/search/presentation/views/widgets/deal.dart';
 import 'package:deals/features/search/presentation/views/widgets/search_view_body.dart';
 
 class SearchView extends StatefulWidget {
-  const SearchView({Key? key}) : super(key: key);
+  const SearchView({super.key});
 
   static const String routeName = 'search';
 
