f21903869a52f4bb854eb4ab2520614293098146
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-03-12 20:32:54 +0200

    Refactor imports in category_tab_bar and custom_error_screen; remove unused app_text_styles.dart import for cleaner code

--- lib/core/widgets/custom_error_screen.dart
+++ lib/core/widgets/custom_error_screen.dart
@@ -1,4 +1,3 @@
-import 'package:deals/core/utils/app_text_styles.dart';
 import 'package:deals/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
