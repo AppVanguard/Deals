5ea4d56dfc9670c295f401344c3499de4c825c35
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-06-20 12:20:28 -0700

    refactor category mapping

--- lib/core/mappers/category_mapper.dart
+++ lib/core/mappers/category_mapper.dart
@@ -1,22 +1,22 @@
 import 'package:deals/core/entities/category_entity.dart';
-import 'package:deals/core/models/category_model/category_model.dart';
+import 'package:deals/core/models/category_model/category_data.dart';
 
 class CategoryMapper {
-  static CategoryEntity mapToEntity(CategoryModel categoryModel) {
+  static CategoryEntity mapToEntity(CategoryData categoryData) {
     return CategoryEntity(
-      id: categoryModel.data?.first.id ?? '',
-      activeCouponCount: categoryModel.data?.first.activeCouponCount ?? 0,
-      averageSavings: categoryModel.data?.first.averageSavings ?? 0,
-      colorCode: categoryModel.data?.first.colorCode ?? '',
-      createdAt: categoryModel.data?.first.createdAt,
-      deletedAt: categoryModel.data?.first.deletedAt,
-      isActive: categoryModel.data?.first.isActive ?? false,
-      isFeatured: categoryModel.data?.first.isFeatured ?? false,
-      order: categoryModel.data?.first.order ?? 0,
-      slug: categoryModel.data?.first.slug ?? '',
-      storeCount: categoryModel.data?.first.storeCount ?? 0,
-      title: categoryModel.data?.first.title ?? '',
-      updatedAt: categoryModel.data?.first.updatedAt,
+      id: categoryData.id ?? '',
+      activeCouponCount: categoryData.activeCouponCount ?? 0,
+      averageSavings: categoryData.averageSavings ?? 0,
+      colorCode: categoryData.colorCode ?? '',
+      createdAt: categoryData.createdAt,
+      deletedAt: categoryData.deletedAt,
+      isActive: categoryData.isActive ?? false,
+      isFeatured: categoryData.isFeatured ?? false,
+      order: categoryData.order ?? 0,
+      slug: categoryData.slug ?? '',
+      storeCount: categoryData.storeCount ?? 0,
+      title: categoryData.title ?? '',
+      updatedAt: categoryData.updatedAt,
     );
   }
 }
