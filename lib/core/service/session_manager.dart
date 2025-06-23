a39fafb429066199d52fd90fac66566b7965fe7d
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-06-23 06:02:28 -0700

    Ensure session dialog has access to SessionCubit

--- lib/core/service/session_manager.dart
+++ lib/core/service/session_manager.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:deals/core/helper_functions/app_router.dart';
 import 'package:deals/core/widgets/session_expired_dialog.dart';
+import 'package:deals/core/manager/cubit/session_cubit/session_cubit.dart';
 
 /// Handles global session related actions like showing the session expired dialog.
 class SessionManager {
@@ -10,7 +12,10 @@ class SessionManager {
     showDialog(
       context: ctx,
       barrierDismissible: false,
-      builder: (_) => const SessionExpiredDialog(),
+      builder: (dialogContext) => BlocProvider.value(
+        value: ctx.read<SessionCubit>(),
+        child: const SessionExpiredDialog(),
+      ),
     );
   }
 }
