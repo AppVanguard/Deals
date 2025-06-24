89885dd63358c56ac01779ce53bd49245e84e3aa
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-06-24 12:34:00 +0300

    Refactor main initialization and move notification permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
 import 'dart:developer';
-import 'package:deals/core/helper_functions/app_router.dart';
-import 'package:deals/features/notifications/data/data_source/notification_local.dart';
-import 'package:deals/core/service/get_it_service.dart';
-import 'package:deals/features/notifications/presentation/manager/cubits/notification_cubit/notifications_cubit.dart';
-import 'package:deals/firebase_options.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+import 'package:deals/core/helper_functions/app_router.dart';
 import 'package:deals/core/manager/cubit/local_cubit/local_cubit.dart';
+import 'package:deals/core/manager/cubit/session_cubit/session_cubit.dart';
+import 'package:deals/core/manager/simple_bloc_observer.dart';
+import 'package:deals/core/service/get_it_service.dart';
 import 'package:deals/core/service/shared_prefrences_singleton.dart';
 import 'package:deals/core/utils/app_colors.dart';
-import 'package:deals/generated/l10n.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:deals/core/manager/simple_bloc_observer.dart';
 import 'package:deals/core/widgets/app_error_widget.dart';
-import 'package:deals/core/manager/cubit/session_cubit/session_cubit.dart';
+import 'package:deals/features/notifications/data/data_source/notification_local.dart';
+import 'package:deals/features/notifications/presentation/manager/cubits/notification_cubit/notifications_cubit.dart';
+import 'package:deals/firebase_options.dart';
+import 'package:deals/generated/l10n.dart';
 
 // 1) Local notifications plugin
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -45,9 +47,9 @@ Future<void> initializeLocalNotifications() async {
   // iOS / Darwin settings (v10+ uses DarwinInitializationSettings):
   const DarwinInitializationSettings
   iosInitSettings = DarwinInitializationSettings(
-    requestAlertPermission: true,
-    requestBadgePermission: true,
-    requestSoundPermission: true,
+    requestAlertPermission: false,
+    requestBadgePermission: false,
+    requestSoundPermission: false,
     // onDidReceiveLocalNotification: (id, title, body, payload) { /* iOS < 10 callback */ },
   );
 
@@ -89,92 +91,78 @@ Future<void> showLocalNotification(RemoteMessage message) async {
   );
 }
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+Future<void> _initHive() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NotificationLocalAdapter());
+  await Hive.openBox<NotificationLocal>('notificationsBox');
+}
 
-  // Global Bloc and error handlers
+void _setupBlocAndErrors() {
   Bloc.observer = SimpleBlocObserver();
   FlutterError.onError = (details) {
     Sentry.captureException(details.exception, stackTrace: details.stack);
     FlutterError.presentError(details);
   };
   ErrorWidget.builder = (details) => AppErrorWidget(details: details);
+}
 
-  // 1) Load env variables
+Future<void> _initSentry(Future<void> Function() runner) {
+  return SentryFlutter.init(
+    (options) {
+      options.dsn = dotenv.env['SENTRY_DSN'] ?? '';
+      options.sendDefaultPii = true;
+    },
+    appRunner: runner,
+  );
+}
+
+void _attachFcmListener() {
+  if (_didAttachFcmListener) return;
+  _didAttachFcmListener = true;
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+    log('Foreground message received: ${message.messageId}');
+    await showLocalNotification(message);
+    if (getIt.isRegistered<NotificationsCubit>()) {
+      getIt<NotificationsCubit>().handleIncomingForegroundMessage(message);
+    }
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    log('Notification caused app to open: ${message.messageId}');
+  });
+}
+
+Widget _buildApp() {
+  return MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (_) => LocaleCubit()),
+      BlocProvider(create: (_) => SessionCubit()),
+    ],
+    child: const Deals(),
+  );
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _setupBlocAndErrors();
+
   await dotenv.load();
 
-  // 2) Initialize Hive
-  await Hive.initFlutter();
-  Hive.registerAdapter(NotificationLocalAdapter());
-  await Hive.openBox<NotificationLocal>('notificationsBox');
+  await Future.wait([
+    _initHive(),
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+    Prefs.init(),
+  ]);
 
-  // 3) Firebase initialize
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // 4) SharedPreferences
-  await Prefs.init();
-
-  // 5) Setup GetIt
   setupGetit();
-
-  // 6) Setup background messages
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  // 7) Init local notifications plugin
   await initializeLocalNotifications();
+  _attachFcmListener();
 
-  // 8) Ask for permissions (iOS)
-  await FirebaseMessaging.instance.requestPermission();
-
-  // 9) Attach the SINGLE onMessage listener if not attached
-  if (!_didAttachFcmListener) {
-    _didAttachFcmListener = true;
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      log("Foreground message received: ${message.messageId}");
-
-      // Show local heads-up or tray notification
-      await showLocalNotification(message);
-
-      // If we have a NotificationsCubit, pass the message to it
-      if (getIt.isRegistered<NotificationsCubit>()) {
-        getIt<NotificationsCubit>().handleIncomingForegroundMessage(message);
-      }
-    });
-  }
-
-  // 10) If user taps a notification that opens the app
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    log("Notification caused app to open: ${message.messageId}");
-    // Optionally do navigation or logic
-  });
-
-  // 11) Finally run the app
+  final runner = () => runApp(_buildApp());
   if (kReleaseMode) {
-    await SentryFlutter.init(
-      (options) {
-        options.dsn = dotenv.env['SENTRY_DSN'] ?? '';
-        options.sendDefaultPii = true;
-      },
-      appRunner: () => runApp(
-        MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (_) => LocaleCubit()),
-            BlocProvider(create: (_) => SessionCubit()),
-          ],
-          child: const Deals(),
-        ),
-      ),
-    );
+    await _initSentry(runner);
   } else {
-    runApp(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_) => LocaleCubit()),
-          BlocProvider(create: (_) => SessionCubit()),
-        ],
-        child: const Deals(),
-      ),
-    );
+    runner();
   }
 }
 
